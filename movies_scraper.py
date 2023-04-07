05a4f678ac0c2e48ef30e2e0eebf518af43cb30d
Tushar Lokhil <82140525+tushar10101@users.noreply.github.com>
2023-04-07 23:11:33 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "531d97ecb3f89eac9b0027ed85f0e90d3e20717d"
+api_key = "281f5ff99a9fc9236ce5a629edabd5f45eeb85c5"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+            url = f"https://link.moviefinders.online/api?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
